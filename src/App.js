8abf9534ec65fa7f18d36a1e7a6333fd9b33c320
muneshgoyal123 <muneshgoyal2012@gmail.com>
2022-08-17 15:46:24 +0530

    todo app commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ToDoList from "./ToDoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import IconButton from '@mui/material/IconButton';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+const App = () => {
+
+    const [inputList, setInputList] = useState("");
+    const [items, setItems] = useState([]); //creating empty array
+
+    const inputEvent = (event) => {
+
+        setInputList(event.target.value);
+
+    };
+
+    const getData = () => {
+        setItems((olditems) => {
+            return [...olditems, inputList];
+        });
+
+        setInputList("");
+
+
+    }
+
+    const deleteItems = (id) => {
+        setItems((olditems) => {
+
+            return olditems.filter((arrElem, index) => {
+                return index !== id
+
+            })
+        })
+    }
+
+    return (
+
+        <>
+            <div class="container bg-light">
+                <div className="col-md-12 text-center">
+                    <button type="button" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+            <div className="main_div">
+                <div className="center_div">
+                    <br />
+
+                    <h1>ToDo List</h1>
+
+                    <br />
+
+                    <input type="text" placeholder="Add Items" onChange={inputEvent} value={inputList} />
+                    {/* <button onClick={getData}> + </button> */}
+                    <IconButton onClick={getData} color="primary" aria-label="add to shopping cart">
+                        <AddShoppingCartIcon />
+                    </IconButton>
+
+                    <ol>
+                        {/* <li>{inputList}</li> */}
+                        {items.map((val, index) => {
+                            return <ToDoList
+                                key={index}
+                                id={index}
+                                text={val}
+                                onSelect={deleteItems}
+
+                            />
+
+                        })}
+
+                    </ol>
+                </div>
+
+            </div>
+
+        </>
+    );
+
+
 }
-
 export default App;
